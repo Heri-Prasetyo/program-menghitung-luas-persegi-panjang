c150dc57c5f3611f7c067981f1f582f492d67ae1
Heri Prasetyo <45659342+Heri-Prasetyo@users.noreply.github.com>
2018-12-22 09:59:29 +0700

    Add files via upload

---
+++ Program C++ Menghitung Luas Persegi Panjang By Heri.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main (){
+int p,l,luas;
+
+cout<<"PROGRAM MENGHITUNG LUAS PERSEGI PANJANG"<<endl;
+cout<<"Masukan Panjang:";
+cin>>p;
+cout<<"Masukan Lebar:";
+cin>>l;
+luas=p*l;
+cout<<"luas persegi panjang adalah = "<<luas;
+return 0;
+}
+
+
+
+
+
+
+
